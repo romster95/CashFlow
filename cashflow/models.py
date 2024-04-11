0d9e12d51809c67594e3277758865a18ee3705d4
Roman Ponepoliak <romanponepolyak@gmail.com>
2024-04-11 18:27:09 +0300

    Added three models: User, Expenses, and Incomes, and performed migration of these models to the database.

--- cashflow/models.py
+++ cashflow/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+
+
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
